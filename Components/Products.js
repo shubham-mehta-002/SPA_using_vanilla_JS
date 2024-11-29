3ca9c8034d5d5edf56982fe8a75d5194a4e0b0f5
Shubham Mehta <134730928+shubham-mehta-002@users.noreply.github.com>
2024-11-29 22:29:53 +0530

    Update Products.js

--- Components/Products.js
+++ Components/Products.js
@@ -1,10 +1,9 @@
-export function Products(){
-    return `<h1>This is child products page </h1>
-                      <nav>
-                        <my-link data-route="product1" data-route-type="private">product1</my-link>
-                        <my-link data-route="product2">product2</my-link>
-                    </nav>
-    <button data-btn-type="navigate" data-route="/contact-us">Navigate Contact-us</button>
-
-                    <div class="outlet"></div>`
+export function Products() {
+  return `<h2>This is products Page </h2>
+                <div>
+                    <my-link data-route="product1">product1</my-link>
+                    <my-link data-route="product2">product2</my-link>
+                </div>
+               
+            <div class="outlet"></div>`;
 }
