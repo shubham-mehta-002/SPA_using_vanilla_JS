c486e72c00c9def969006efabb184e0c348ccf85
Shubham Mehta <134730928+shubham-mehta-002@users.noreply.github.com>
2024-11-29 22:29:35 +0530

    Update Home.js

--- Components/Home.js
+++ Components/Home.js
@@ -1,9 +1,9 @@
 export function Home() {
-  return `<h1>This is home page....</h1>
-                    <nav>
+  return `<h2>This is home page....</h2>
+                    <div>
                         <my-link data-route="products">Products</my-link>
-                        <my-link data-route="about">About</my-link>
-                        <my-link data-route="contact-us" data-route-type="private">Contact-us</my-link>
-                    </nav>
+                        <my-link data-route="services">Services</my-link>
+                        <my-link data-route="faq">FAQ</my-link>
+                    </div>
                     <div class="outlet"></div>`;
 }
