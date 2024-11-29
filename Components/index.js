4d6d399814e9977edd73922745a82dbcbda0ce23
Shubham Mehta <134730928+shubham-mehta-002@users.noreply.github.com>
2024-11-29 22:30:20 +0530

    Update index.js

--- Components/index.js
+++ Components/index.js
@@ -2,3 +2,7 @@ export { Profile } from "./Profile.js"
 export { About } from "./About.js"
 export { Home } from "./Home.js"
 export { Products } from "./Products.js"
+export { Services} from "./Services.js"
+export { Login } from "./Login.js"
+export { Logout } from "./Logout.js"
+
