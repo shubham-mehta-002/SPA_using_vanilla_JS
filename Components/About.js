4ec49d1d29c4f72f73f4efb80f91a414ce607833
Shubham Mehta <134730928+shubham-mehta-002@users.noreply.github.com>
2024-11-29 22:29:15 +0530

    Update About.js

--- Components/About.js
+++ Components/About.js
@@ -1,6 +1,6 @@
 export function About() {
-  return `<h1>This is about page....</h1>
-                  <my-link data-route="about1">About 1</my-link>
-                  <my-link data-route="about2">About 2</my-link>
-                  <div id="d2.0" class="outlet"></div>`;
+  return `<h2>This is about page....</h2>
+              <my-link data-route="about1">About 1</my-link>
+              <my-link data-route="about2">About 2</my-link>
+          <div id="d2.0" class="outlet"></div>`;
 }
